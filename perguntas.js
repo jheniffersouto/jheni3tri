68bbf7867964fc699642c6adda45b14101068bd5
jheniffersouto <134400666+jheniffersouto@users.noreply.github.com>
2024-10-04 10:59:53 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,71 +1,70 @@
 criaCartao(
     'Supernatural',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Em que ano a série estreiou?',
+    '2005'
 )
 
 criaCartao(
     'Supernatural',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o nome do pai dos irmãos Wincheter?',
+    'Jhon Winchester'
 )
 
 criaCartao(
     'Supernatural',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+  'O que Sam e Dean usam pra exorcizar o demônios',
+  'Sal',
+)
+criaCartao(
+    'Supernatural',
+    'Qual é o carro dos irmãos Winchester',
+    'Impala',
+)   
+
+criaCartao(
+    'Supernatural',
+    'Qual é o nome do líder os Cavaleiros o Apocalipse, representando a Peste',
+    'Pestilência'
 )
 
 criaCartao(
     'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual é o nome do anjo que se torna temporariamente um aliado dos irmãos Winchester',
+    'castiel'
 )
 
 criaCartao(
     'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual é o primeiro irmãos a ser ressuscitado após sua morte permanente na série?',
+    'Dean Winchester'
 )
 
 criaCartao(
     'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'O que acontece com o irmãos winchester a cada 10 anos devio a um acordo feito por seu pai?',
+    'Um deles é enviado para o inferno'
 )
 
 criaCartao(
     'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual é o apelido de Sam winchester dado por Crowley, o Rei do inferno?',
+    'Alce'
 )
 
 criaCartao(
     'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual é o nome do cão de caça de Bobby Singer',
+    'Rumsfeld'
 )
 
 criaCartao(
     'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quem é o caçcador que se torna um aliado frequente dos irmãos Winchester e é conhecido por seu conhecimento sobre o sobrenatural',
+    'Bobby Singer'
 )
 
 criaCartao(
     'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-
-criaCartao(
-    'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-
-criaCartao(
-    'Supernatural',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual é o irmão mais velho dos irmãos Winchester',
+    'Dean'
 )
